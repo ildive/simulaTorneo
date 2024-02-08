0545df287a43861f59d7748c5d201f2f71b7c188
gabriel.divera <gabriel.divera@LAB22PC12>
2024-02-08 11:36:18 +0100

    Aggiungere i file di progetto.

---
+++ EsercizioClassi/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace EsercizioClassi
+{
+    internal class Program
+    {
+        static dou
+        static int minutiAttuali;
+        static void Main(string[] args)
+        {
+            Squadra cerignola = new Squadra("Audace Cerignola");
+            Squadra juve = new Squadra("Juventus");
+        }
+
+        static int numeroRandom(int n0, int n1)
+        {
+            Random rand = new Random();
+            int nRandom = rand.Next(n0, n1+1);
+            return nRandom;
+        }
+
+        static void scorriTempo()
+        {
+
+
+            for (int i = 0; i < 90; i++)
+            {
+
+            }
+        }
+
+
+        
+
+        static void assegnaGoalSquadraRandom()
+        {
+            
+        }
+        static void iniziaPartita(Squadra squadra1, Squadra squadra2)
+        {
+            minutiAttuali = 0;
+            assegnaGoalSquadraRandom(squadra1, squadra2);
+
+            if(numeroRandom(0, 1) == 0) {
+                squadra1.goalFatto();
+            }
+        }
+    }
+}
---
+++ EsercizioClassi/Squadra.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsercizioClassi
+{
+    internal class Squadra
+    {
+        private string nomeSquadra;
+        private int goalsFatti;
+        private int goalsRicevuti;
+        private int partiteVinte;
+        private int partitePerse;
+        private int partitePareggiate;
+        private int punti;
+
+        public Squadra(string nomeSquadra)
+        {
+            goalsFatti = 0;
+            goalsRicevuti = 0;
+            partiteVinte = 0;
+            partitePerse = 0;
+            partitePareggiate = 0;
+            this.nomeSquadra = nomeSquadra;
+        }
+
+        
+        /*  Getter  */
+
+        public string getNome()
+        {
+            return nomeSquadra;
+        }
+
+        public int getGoalsFatti()
+        {
+            return goalsFatti;
+        }
+
+        public int getGoalsRicevuti()
+        {
+            return goalsRicevuti;
+        }
+
+        public int getPartiteVinte()
+        {
+            return partiteVinte;
+        }
+
+        public int getPartitePerse()
+        {
+            return partitePerse;
+        }
+
+        public int getPartitePareggiate()
+        {
+            return partitePareggiate;
+        }
+
+        public int getPunti()
+        {
+            punti = (partiteVinte*3) + partitePareggiate;
+            return punti;
+        }
+
+        public void goalFatto()
+        {
+            goalsFatti++;
+        }
+
+        public void goalSubito()
+        {
+            goalsRicevuti++;
+        }
+
+        public void vinto()
+        {
+            partiteVinte++;
+        }
+
+        public void perso()
+        {
+            partitePerse++;
+        }
+
+        public void pareggio()
+        {
+            partitePareggiate++;
+        }
+    }
+    
+
+}
